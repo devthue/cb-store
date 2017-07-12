4402e63243530fd771b4fa74e27d5ff09257f6e3
Dinh Nhat Bang <dinhnhatbang@gmail.com>
2017-07-12 13:52:00 +0700

    Update default configs

--- src/configs/default.sample.php
+++ src/configs/default.sample.php
@@ -1,7 +1,30 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: BangDinh
- * Date: 7/3/17
- * Time: 10:46
- */
+
+return [
+    'debug' => true,
+    'cors' => [
+        'allowedOrigins' => ['*']
+    ],
+    'application' => [
+        'title' => 'Micro service API',
+        'description' => 'Micro service API.',
+        'baseUri' => '/',
+        'viewsDir' => __DIR__ . '/../views/',
+        'loggerUrl' => __DIR__ . '/../logs/errors.log',
+    ],
+    'authentication' => [
+        'secret' => 'this_should_be_changed',
+        'accesstrustedkey'=> 'hello',
+        'expirationTime' => 86400 * 7, // One week till token expires
+    ],
+    'services' => [
+        'oss' => [
+            'basicToken' => 'Basic oss_basic_token',
+            'url' => 'http://oss.seedcom.vn/',
+            'action' => [
+                'login' => 'sso/oauth/token',
+                'userInformation' => 'hrapi/employee/getuserinfo'
+            ]
+        ]
+    ]
+];
--- src/configs/server.sample.php
+++ src/configs/server.sample.php
@@ -1,7 +1,20 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: BangDinh
- * Date: 7/3/17
- * Time: 10:46
- */
+
+return [
+    'database' => [
+        'adapter' => 'Mysql',
+        'host' => 'mariadb',
+        'username' => 'root',
+        'password' => '',
+        'dbname' => 'cdf_store',
+        'options' => [
+            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
+        ]
+    ],
+    'redis' => [
+        'host' => 'redis',
+        'port' => 6379,
+        'persistent' => true,
+        'lifetime' => 860000
+    ]
+];
